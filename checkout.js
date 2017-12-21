e915a19924439847ae9ffd3f77eba408de491e6e
a063mg <alekseev.artem700@gmail.com>
2017-12-21 17:03:15 +0300

    testing github

--- checkout.js
+++ checkout.js
@@ -1,5 +1,5 @@
 function checkout(data){
-	$(".g-recaptcha").remove()
+	// $(".g-recaptcha").remove()
 	$('#order_billing_name').val(data['name'])
 	$('#order_email').val(data['email'])
 	$('#order_tel').val(data['phone'])
