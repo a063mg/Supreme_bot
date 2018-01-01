51e42d02798ea6a28a15db969851384f77c6b430
a063mg <alekseev.artem700@gmail.com>
2018-01-01 23:49:43 +0300

    site updated

--- popup.js
+++ popup.js
@@ -5,10 +5,15 @@ if(page){
 }
 
 function loadItems(){
-	var obj = JSON.parse(localStorage["data"]);
-	Object.keys(obj).forEach(function(item, i, arr) {
-		document.getElementById('items_table').innerHTML += '<tr id="'+item+'"><td>'+item+'</td><td>'+obj[item]["keyword"]+'</td><td>'+obj[item]["color"]+'</td><td>'+obj[item]["size"]+'</td><td>'+obj[item]["category"]+'</td><td id="delete" name="'+item+'" style="background: red;">Delete</td></tr>';
-	});
+	if (localStorage["data"] == undefined) {
+		localStorage["data"] = JSON.stringify({});
+	}
+	else{
+		var obj = JSON.parse(localStorage["data"]);
+		Object.keys(obj).forEach(function(item, i, arr) {
+			document.getElementById('items_table').innerHTML += '<tr id="'+item+'"><td>'+item+'</td><td>'+obj[item]["keyword"]+'</td><td>'+obj[item]["color"]+'</td><td>'+obj[item]["size"]+'</td><td>'+obj[item]["category"]+'</td><td id="delete" name="'+item+'" style="background: red;">Delete</td></tr>';
+		});
+	}
 }
 
 function clear() {
@@ -19,15 +24,18 @@ function clear() {
 loadItems();
 
 $('[id=delete]').click(function(elem){
-	var obj = JSON.parse(localStorage["data"]);
-	var id = elem.target.getAttribute('name');
-	delete obj[id];
-	new_list = {}
-	Object.keys(obj).forEach(function(item, i, arr) { 
-		new_list[i+1] = obj[item];
-	});
-	localStorage["data"] = JSON.stringify(new_list);
-	window.location = 'popup.html';
+	if (localStorage["data"] == undefined) {
+		localStorage["data"] = JSON.stringify({});
+	}
+		var obj = JSON.parse(localStorage["data"]);
+		var id = elem.target.getAttribute('name');
+		delete obj[id];
+		new_list = {}
+		Object.keys(obj).forEach(function(item, i, arr) { 
+			new_list[i+1] = obj[item];
+		});
+		localStorage["data"] = JSON.stringify(new_list);
+		window.location = 'popup.html';
 });
 
 
@@ -36,6 +44,7 @@ document.getElementById('items').onclick = () => window.location = 'items.html';
 document.getElementById('view').onclick = () => {
 	window.open("/site/test.html");
 };
+
 document.getElementById('clear').onclick = () => { 
 	clear();
  };
@@ -43,9 +52,12 @@ document.getElementById('clear').onclick = () => {
 
 function cop(){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var obj = JSON.parse(localStorage["data"]);
-  	var url = 'http://www.supremenewyork.com/shop/all/' + obj[Object.keys(obj)[0]]["category"];
-  	chrome.runtime.sendMessage({msg:'go', url: url, id: tabs[0].id}, function submitForm(par){  console.log(par); });
-  	console.log(1);
+  	if (localStorage["data"] == undefined) {
+		localStorage["data"] = JSON.stringify({});
+	}
+	    var obj = JSON.parse(localStorage["data"]);
+	  	var url = 'http://www.supremenewyork.com/shop/all/' + obj[Object.keys(obj)[0]]["category"];
+	  	chrome.runtime.sendMessage({msg:'go', url: url, id: tabs[0].id}, function submitForm(par){  console.log(par); });
+	  	console.log(1);
   });
 }
--- site/site.js
+++ site/site.js
@@ -7,7 +7,6 @@
           var arr = txt.split('\n');
           for(var i = 0; i < arr.length; i++){
               var data = arr[i].split(';');
-              console.log(arr[i]);
               document.getElementById('row').innerHTML += '<div class="card"><img src='+data[0]+' /><p class="card-text">'+data[1]+'</p><p class="hidden category">'+data[3]+'</p><h1 class="card-price">'+data[2]+'</h1><button class="btn1">Add</button></div>';
           }
         }
@@ -16,7 +15,10 @@
       xmlhttp.send();
 
   		var input1 = document.getElementById("input-size");
-  		var lert = document.getElementsByClassName("alert")[0];
+  		var danger = document.getElementsByClassName("alert-danger")[0];
+      var danger1 = document.getElementsByClassName("alert-danger")[1];
+      var success = document.getElementsByClassName("alert-success")[0];
+      var lert1 = document.getElementsByClassName("not")[0];
   		var input2 = document.getElementById("input-color");
   		var close = document.getElementById("close");
   		var layer = document.getElementById("layer");
@@ -42,36 +44,66 @@
   			}
   		};
 
-      $(document).ready(function(){
+    $(document).ready(function(){
         $('.btn1').click(function(elem){
-        console.log(1);
-        var id = elem.target.parentElement;
-            
-        lert.style.display = 'none';
-        input1.value = "";
-        input1.style.borderColor = "rgb(255, 0, 84)";
-        input2.value = "";
-        input2.style.borderColor = "rgb(255, 0, 84)";
-        
-        var text = id.getElementsByClassName("card-text")[0].innerText;
-        var price = id.getElementsByClassName("card-price")[0].innerText;
-        var category  = id.getElementsByClassName("category")[0].innerText;
-        var url = id.getElementsByTagName("img")[0].src;
+          var id = elem.target.parentElement;
+              
+          danger.style.display = 'none';
+          success.style.display = 'none';
+          danger1.style.display = 'none';
+          input1.value = "";
+          input1.style.borderColor = "rgb(255, 0, 84)";
+          input2.value = "";
+          input2.style.borderColor = "rgb(255, 0, 84)";
+          
+          var text = id.getElementsByClassName("card-text")[0].innerText;
+          var price = id.getElementsByClassName("card-price")[0].innerText;
+          var category  = id.getElementsByClassName("category")[0].innerText;
+          var url = id.getElementsByTagName("img")[0].src;
 
-        console.log(url);
+          document.getElementsByClassName("preview-card-text")[0].innerText = text;
+          document.getElementsByClassName("preview-categoty")[0].innerText = category;
+          document.getElementsByClassName("preview-image")[0].src = url;
+          document.getElementsByClassName("preview-card-price")[0].innerText = price;
 
-        document.getElementsByClassName("preview-card-text")[0].innerText = text;
-        document.getElementsByClassName("preview-card-text")[1].innerText = category;
-        document.getElementsByClassName("preview-image")[0].src = url;
-        document.getElementsByClassName("preview-card-price")[0].innerText = price;
-
-        $("#layer").fadeIn("slow")
-      });
-
+          $("#layer").fadeIn("slow")
+        });
        $('.add-button').click(function(elem){ 
             if (input2.value == "" ||  input1.value == ""){
-                lert.style.display = 'block';
+                danger.style.display = 'block';
+                success.style.display = 'none';
+                danger1.style.display = 'none';
             }
+            else{
+              if (localStorage["data"] == undefined) {
+                localStorage["data"] = JSON.stringify({});
+              }
+              var obj = JSON.parse(localStorage["data"]);
+              var index = Object.keys(obj).length;
+              if (index > 3){
+                localStorage["data"] = JSON.stringify({});
+              }
+              if (index < 3){
+                var id = elem.target.parentElement;
+
+                var keyword = id.getElementsByClassName("preview-card-text")[0].innerText;
+                var category  = id.getElementsByClassName("preview-categoty")[0].innerText;
+
+                var index = index+1;
+
+                obj[index] = {"keyword": keyword, "category": category, "color": input2.value, "size": input1.value};
+                localStorage["data"] = JSON.stringify(obj);
+                danger.style.display = 'none';
+                danger1.style.display = 'none';
+                success.style.display = 'block';
+                $('.alert-success').delay(1000).fadeOut("slow");
+              }
+              else{
+                danger1.style.display = 'block';
+                danger.style.display = 'none';
+                success.style.display = 'none';
+              }
+          }
        });
     });
 
